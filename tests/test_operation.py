dd235a28b862837ed1c8304896e56d2cf4673c61
ankitkrchoudhary” <ankitkrchoudhary1@gmail.com>
2025-04-17 21:35:38 +0530

    unit test cases updated

---
+++ tests/test_operation.py
@@ -0,0 +1,11 @@
+from src.math_operations import add,sub
+
+def test_add():
+    assert add(2,3)==3
+    assert add(1,3)==4
+
+def test_sub():
+    assert sub(4,3)==1
+    assert sub(7,4)==3
+    assert sub(8,9)==-1
+    
